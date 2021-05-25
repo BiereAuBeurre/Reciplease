19570b5204dfde112e4b9464e9cc0acfd8fb0dc0
manon <manon.dev.ios@gmail.com>
2021-05-25 17:22:52 +0200

    edamam service added

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -13,9 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        AF.request("https://api.edamam.com/search?q=chicken,ham,curry&app_key=b5144453065bd0a94728a7da37aa3548&app_id=d698f1a4&ingredient=food&food=label").response { response in
-            debugPrint(response)
-        }
     }
 
 }
